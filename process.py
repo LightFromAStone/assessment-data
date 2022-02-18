20852b760be5751a62b5b4028338f89c86e48f4b
LightFromAStone <github@lightfromastone.me>
2022-02-18 09:15:22 -0700

    Completed Part 1

--- process.py
+++ process.py
@@ -1,12 +1,12 @@
-log_file = open("um-server-01.txt")
+log_file = open("um-server-01.txt") #opens the text file and assigns it to log_file
 
 
-def sales_reports(log_file):
-    for line in log_file:
-        line = line.rstrip()
-        day = line[0:3]
-        if day == "Tue":
-            print(line)
+def sales_reports(log_file): # declares a funcntion
+    for line in log_file: # loop line by line over the text file
+        line = line.rstrip() # removes trailing characters, specifically whitespace
+        day = line[0:3] # selects the portion of the line that contains the day
+        if day == "Mon": # checks if day selected is Mon (originally Tue)
+            print(line) # prints that line if it is Mon 
 
 
-sales_reports(log_file)
+sales_reports(log_file) # invocation of the function
