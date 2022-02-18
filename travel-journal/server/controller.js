627e2fe2bf65776be315df513eee311ccb573580
LightFromAStone <github@lightfromastone.me>
2022-02-18 11:18:54 -0700

    Completed Part 3

--- travel-journal/server/controller.js
+++ travel-journal/server/controller.js
@@ -1,4 +1,14 @@
+require('dotenv').config();
+const Sequelize =  require('sequelize');
 
+const sequelize = new Sequelize(
+    process.env.CONNECTION_STRING, {
+        dialect: 'postgress',
+        dialectOptions: {
+            ssl: { rejectUnauthorized: false }
+        }
+    }
+);
 
 module.exports = {
     seed: (req, res) => {
@@ -11,7 +21,12 @@ module.exports = {
                 name varchar
             );
 
-            *****YOUR CODE HERE*****
+            CREATE TABLE cities (
+                city_id SERIAL PRIMARY KEY,
+                name VARCHAR(100),
+                rating INT,
+                country_id INT REFERENCES countries(country_id)
+            );
 
             insert into countries (name)
             values ('Afghanistan'),
@@ -213,5 +228,40 @@ module.exports = {
             console.log('DB seeded!')
             res.sendStatus(200)
         }).catch(err => console.log('error seeding DB', err))
+    },
+
+    getCountries: (req, res) => {
+        sequelize.query(`
+            SELECT * FROM countries;
+        `).then(dbRes => res.status(200).send(dbRes[0]))
+          .catch(err => console.log('error getting countries from DB', err));
+    },
+
+    createCity: (req, res) => {
+        const {name, rating, countryId} = req.body;
+        sequelize.query(`
+            INSERT INTO cities (name, rating, country_id)
+            VALUES ('${name}', ${rating}, ${countryId});
+        `).then(dbRes => res.status(200).send(dbRes[0]))
+          .catch(err => console.log('error creating a city in DB', err));
+    },
+
+    getCities: (req, res) => {
+        sequelize.query(`
+            SELECT cities.city_id, cities.name AS city, cities.rating, countries.country_id, countries.name AS country
+            FROM cities
+            JOIN countries
+            ON cities.country_id = countries.country_id;
+        `).then(dbRes => res.status(200).send(dbRes[0]))
+          .catch(err => console.log('error getting a cities from DB', err));
+    },
+
+    deleteCity: (req, res) => {
+        const {id} = req.params;
+        sequelize.query(`
+            DELETE FROM cities
+            WHERE city_id = ${id};
+        `).then(dbRes => res.status(200).send(dbRes[0]))
+          .catch(err => console.log('error getting a cities from DB', err));
     }
 }
